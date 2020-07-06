f6bc950301a8e0c35590e2b3d39649c8fd5b7538
Tiger Kaovilai <passawit.kaovilai@gmail.com>
2020-07-06 17:32:37 +0700

    white shadow

--- ad.js
+++ ad.js
@@ -69,7 +69,7 @@ function populateJSON(elementid, adkey, adjson){
             transition-timing-function: ease-out;\
         }\
         img.adjs:hover {\
-            box-shadow: 0px 0px 40px green;\
+            box-shadow: 0px 0px 40px white;\
             -webkit-transform: scale(1.05);\
             animation: wiggle 1s infinite;\
             \
